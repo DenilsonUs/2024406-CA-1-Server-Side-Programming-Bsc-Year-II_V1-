1a8c5627be76db28caf0433c0641b0a920fdf708
Denilson <urias4141@gmail.com>
2025-10-23 23:44:13 +0100

    install express
    install and initial express.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 // Import the database connection
-var connection = require("./database");
+var connection = require("./database.js");
 
 // Middleware to read form data
 app.use(express.urlencoded({ extended: true }));
