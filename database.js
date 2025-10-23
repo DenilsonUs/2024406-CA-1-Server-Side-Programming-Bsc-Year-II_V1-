188478937a41a8c1f77afe4d4eb531797085dc69
Denilson <urias4141@gmail.com>
2025-10-23 23:31:22 +0100

    DataBase
    Create the data base connection

---
+++ database.js
@@ -0,0 +1,12 @@
+var mysql = require("mysql");
+
+// Create the connection to the MySQL database
+var connection = mysql.createConnection({
+  host: "localhost", // MySQL server location
+  user: "root", // your MySQL username
+  password: "lopez2424", // your MySQL password (leave empty if none)
+  database: "user_form_db", // name of your database
+});
+
+// Export this connection so it can be used in server.js
+module.exports = connection;
