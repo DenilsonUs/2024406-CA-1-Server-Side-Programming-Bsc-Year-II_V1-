98229f99bc5eef7e3374d1a609243c897c66e16c
Denilson <urias4141@gmail.com>
2025-10-24 20:07:39 +0100

    Show data base in the terminal

--- database.js
+++ database.js
@@ -1,12 +1,25 @@
+// Import the mysql2 library
 var mysql = require("mysql2");
 
 // Create the connection to the MySQL database
 var connection = mysql.createConnection({
   host: "localhost", // MySQL server location
   user: "root", // your MySQL username
-  password: "lopez2424", // your MySQL password (leave empty if none)
-  database: "user_form_db", // name of your database
+  password: "lopez2424", // your MySQL password
+  database: "user_form_db", // your database name
 });
 
-// Export this connection so it can be used in server.js
+// Connect to the database
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Database Connected");
+
+  // Run a simple SELECT query to get all rows
+  connection.query("SELECT * FROM user_data", function (err, results) {
+    if (err) throw err;
+
+    console.log("Data from user_data table:");
+    console.table(results); // display rows in a nice table format
+  });
+});
 module.exports = connection;
